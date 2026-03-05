199dac531ae9a00284ff26cdded5ad74b41a0dab
Awizp <ridvishnu@gmail.com>
2026-03-05 09:23:58 +0530

    fix: project 04 title added

--- src/projects/project04/components/HeroPage.jsx
+++ src/projects/project04/components/HeroPage.jsx
@@ -5,6 +5,7 @@ import VideoShow from "./VideoShow";
 const HeroPage = () => {
     return (
         <div className="w-full">
+            <title>Welcome to CyberTube | Watch Education content and attain your dream career</title>
             <VideoProvider>
                 <Navbar />
 
